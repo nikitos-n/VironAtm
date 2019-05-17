9da33b679e57f094729b17b342d2aa52b7822bd2
Nikita Nikolskiy <Nikita27142914@yandex.ru>
2019-05-17 21:45:32 +0300

    Сделал теперь нормыльный тест EventEmmiter

--- src/EventEmmiter.test.js
+++ src/EventEmmiter.test.js
@@ -2,12 +2,14 @@ import EventEmmiter from './EventEmmiter'
 
 test('Testing .on in EventEmmiter', () => {
     let EvEm = new EventEmmiter();
-    EvEm.emit("test");
-    let f=EvEm.on("test",()=>console.log("Hello world!"));
-    EvEm.on("test",(a)=>console.log(a));
-    EvEm.emit("test");
-    EvEm.emit("test",2);
-    f();
-    EvEm.emit("test");
-    EvEm.clearTable();
+    expect(typeof EvEm).toBe('object');
+    expect(typeof EvEm.eventTable).toBe('object');
+    let f = EvEm.on("test", () => console.log("Hello world!"));
+    // EvEm.emit("test");
+    // EvEm.on("test", (a) => console.log(a));
+    // EvEm.emit("test");
+    // EvEm.emit("test", 2);
+    // f();
+    // EvEm.emit("test");
+    // EvEm.clearTable();
 })

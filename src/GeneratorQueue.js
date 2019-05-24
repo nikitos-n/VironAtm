f450bebb199209f0b8c0a12d6d8756b812a7e9f0
Nikita <Nikita27142914@yandex.ru>
2019-05-24 11:49:16 +0300

    Еще пока тесты

--- src/GeneratorQueue.js
+++ src/GeneratorQueue.js
@@ -1,21 +1,20 @@
-export default function createGeneratorQueue(queue_1, queueUI_1, n, m, i) { //Генерируем очередь
-    function increaseQueue(n, m) { //Наращиваем очередь
-        setTimeout(() => {
-            if (queue_1) {
-                queue_1.IncreaseAmount();
-                increaseQueue(n, m);
-            }
-        },
-            createInterval(n, m))
-    }
-    increaseQueue(n, m);
-
+export function createGeneratorQueue(queue_1, queueUI_1, n, m, i) { //Генерируем очередь
+    increaseQueue(queue_1, queueUI_1, n, m, i);
     return queue_1;
 }
 
-function createInterval(min, max) {
+export function increaseQueue(queue_1, queueUI_1, n, m, i) { //Наращиваем очередь
+    setTimeout(() => {
+        if (queue_1) {
+            queue_1.IncreaseAmount();
+            increaseQueue(queue_1, queueUI_1, n, m, i);
+        }
+    },
+        createInterval(n, m))
+}
+
+export function createInterval(min, max) {
     let rand = (min + Math.random() * (max + 1 - min));
     rand = Math.floor(rand) * Math.pow(10, 3);
     return rand;
 }
-//#endregion

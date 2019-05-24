35e12bc00c29b4c280f93310e3423e0c6486e5f5
Nikita Nikolskiy <Nikita27142914@yandex.ru>
2019-05-24 21:13:09 +0300

    Все еще эти тесты

--- src/addAtm.test.js
+++ src/addAtm.test.js
@@ -3,6 +3,7 @@ import Atm from './Atm';
 import AtmUI from './AtmUI';
 import Queue from './Queue';
 import QueueUI from './QueueUI';
+import Person from './Person'
 document.body.innerHTML = `<div id="forUIAtm" class="atmUIFlex-container">`
 jest.useFakeTimers();
 
@@ -18,6 +19,7 @@ test('Testing .addAtm in App', () => {
     expect(app1.AtmArray[app1.AtmArray.length - 1].eventTable.hasOwnProperty('unsubscribeServedAtm')).toBeTruthy();
     expect(app1.AtmArray.length).toBe(1);
     expect(app1.AtmArrayUI.length).toBe(1);
+    app1.AtmArray[app1.AtmArray.length - 1].unsubscribeState();
 });
 
 test('Testing .realizeAtm in App', () => {
@@ -26,12 +28,29 @@ test('Testing .realizeAtm in App', () => {
     app1.AtmArray.push(atm1);
     app1.AtmArrayUI.push(atm1UI);
     jest.advanceTimersByTime(2000);
-    // expect(app1.realizeAtm).toHaveBeenCalledTimes(1);
-
     let queue1 = new Queue();
-    queue1.PersonAmount.length = 2;
+    let person1 = new Person();
+    queue1.PersonAmount.push(person1);
     app1.QueueArray.push(queue1);
-    // app1.renderProcess = jest.fn();
     app1.renderProcess(app1.AtmArray, app1.QueueArray, 1);
-    // expect(app1.renderProcess).toHaveBeenCalledTimes(1);
+
+    app1.renderProcess = jest.fn();
+    app1.renderProcess(app1.AtmArray, app1.QueueArray, 1);
+    expect(app1.renderProcess).toHaveBeenCalledTimes(1);
+
+    app1.startProcess = jest.fn();
+    app1.startProcess(app1.AtmArray, app1.QueueArray, 1);
+    expect(app1.startProcess).toHaveBeenCalledTimes(1);
+});
+
+test('Testing .closeAtm in App', () => {
+    let atm1 = new Atm();
+    let atm1UI = new AtmUI();
+    app1.AtmArray.push(atm1);
+    app1.AtmArrayUI.push(atm1UI);
+    let closeAtm = atm1UI.ChildAtmDivClose;
+    app1.startProcess(2, 3);
+    app1.addAtm = jest.fn();
+    app1.addAtm();
+    expect(app1.addAtm).toHaveBeenCalledTimes(1);
 });
